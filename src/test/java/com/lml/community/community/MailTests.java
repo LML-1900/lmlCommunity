cd6f0d8c44ce1bee432b58cf33e05f9c7cd05cac
LML-1900 <m.luo1900@gmail.com>
2023-07-09 23:46:40 +0800

    debug and add sendEmail function

--- src/test/java/com/lml/community/community/MailTests.java
+++ src/test/java/com/lml/community/community/MailTests.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
 
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
@@ -12,8 +15,21 @@ public class MailTests {
     @Autowired
     private MailClient mailClient;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
     public void testTextMail() {
         mailClient.sendMail("18373771@buaa.edu.cn", "TEST", "Welcome to lml's community");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("18373771@buaa.edu.cn", "HTML TEST", content);
     }
 }
